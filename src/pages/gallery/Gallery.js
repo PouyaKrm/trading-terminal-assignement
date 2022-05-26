6623c9bbfdbd6ea9d85cbaf5f10191ebb3b22aa2
Pouya Kermanshahi <103865529+pouya-kermanshahi@users.noreply.github.com>
2022-05-26 17:02:13 +0430

    Implements gallery layout

--- src/pages/gallery/Gallery.js
+++ src/pages/gallery/Gallery.js
@@ -1,10 +1,33 @@
-import "Gallery.scss";
+import "./Gallery.scss";
+import React from "react";
 
 const Gallery = () => {
+
+    const items = Array.from(Array(10).keys());
+    const addItems = () => {
+        let items = [];
+        for (let i = 0; i < 10; i++) {
+            const item = (
+                <div style={{backgroundColor: "red", border: "1px solid black", width: "100%", height: "100%"}}>
+                </div>
+            );
+            items.push(item)
+        }
+        return items;
+    };
+
     return (
-        <>
-        </>
+        <div className="container">
+            {
+                items.map((e, index) => {
+                    return (
+                        <div key={index} className="gallery-item">
+                        </div>
+                    )
+                })
+            }
+        </div>
     )
 };
 
-export default {Gallery};
+export default Gallery;
